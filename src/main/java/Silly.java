3bc2a9884ed9eca59ffcb820e58c724220035bd5
yanbo <yanbocheng01234@gmail.com>
2022-09-14 12:59:19 -0400

    Implement TODO's

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,6 +79,10 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+    public Silly(String first, String second) {
+        //this method sets this.name to the concatenation of first and second
+        this.name = first + second;
+    }
 
 
 
@@ -116,7 +120,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +138,8 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        //return the name of this silly
+        return this.name;
     }
 
     /**
@@ -161,6 +167,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return o.toString().equals(this.name);
     }
 
     /**
@@ -194,6 +201,7 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        return this.name.length() - other.toString().length();
     }
 
     /*
